114e4f7237529ac9447a4fc5691250f923b12b61
rahul-yadav20 <rahul.ydv0927@gmail.com>
2025-02-15 15:16:51 +0530

    Server - Set DB Local

--- backend/database.js
+++ backend/database.js
@@ -3,7 +3,7 @@ const mongoose = require('mongoose');
 // Mongo Database connection URI
 // CloudNotebook is the database
 // automatically created in mongodb when you insert a document
-const mongoUri = 'mongodb+srv://rahulydv0927:2rsyME20MIbHzYyp@cluster0.wwmt31v.mongodb.net/CloudNotebook';
+const mongoUri = 'mongodb://localhost:27017/CloudNotebook';
 
 const connectFunction = () => {
 
